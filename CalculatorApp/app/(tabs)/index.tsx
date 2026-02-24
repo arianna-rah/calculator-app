754fd731df6a1bf8b02023d85fa8660a3a076878
hannahrajan <hannah.rajan@gmail.com>
2026-02-24 12:49:12 -0500

    Added Calendar, Centered Sliders
    Bugs - shows '+' symbol but doesn't change with the date selector

--- CalculatorApp/app/(tabs)/index.tsx
+++ CalculatorApp/app/(tabs)/index.tsx
@@ -1,19 +1,36 @@
+import { Calendar } from "@/components/calendar";
 import { SliderExample } from "@/components/slider";
-import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const operations = ["  +  ", "  -  ", "  x  ", "  /  ", "  %  ", "  ^  "];
+function CalculateOperatorGivenTime(date: Date): String {
+  let year: number = date.getFullYear();
+  let month: number = date.getMonth();
+  let day: number = date.getDate();
+  return operations[day % 6];
+}
 
 export default function HomeScreen() {
+  let today = new Date();
   const [sliderVal, setSliderVal] = useState(0.015625);
+  const [calendarVal, setCalendarVal] = useState(today);
+  const [operation, setOperation] = useState(CalculateOperatorGivenTime(today));
   const [sliderVal2, setSliderVal2] = useState(0.015625);
 
-  const dateOperation = () => {
-    return sliderVal * sliderVal2; // PLACEHOLDER BC I HAVENT STARTED THE DATE SELECTOR YET
-  };
+  useEffect(() => {
+    setOperation(CalculateOperatorGivenTime(calendarVal));
+  }, [calendarVal]);
 
   return (
     <View style={styles.container}>
       <SliderExample value={sliderVal} onValueChange={setSliderVal} />
-
+      <Calendar
+        value={calendarVal}
+        onValueChange={setCalendarVal}
+        operation={operation}
+      />
+      <Text>{calendarVal.toDateString()}</Text>
       <SliderExample value={sliderVal2} onValueChange={setSliderVal2} />
     </View>
   );
---
+++ CalculatorApp/components/calendar.tsx
@@ -0,0 +1,111 @@
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { useEffect, useState } from "react";
+import { Alert, Modal, Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+interface DatePickerProps {
+  value: Date;
+  onValueChange: (val: Date) => void;
+  operation: string;
+}
+
+export function Calendar({ value, onValueChange, operation }: DatePickerProps) {
+  let today = new Date();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(value);
+
+  useEffect(() => {
+    setSelectedDate(value);
+  }, [value]);
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.centeredView}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <DateTimePicker
+                mode={"date"}
+                value={value}
+                onValueChange={onValueChange}
+              />
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Return to Calculator</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+        <Pressable
+          style={[styles.button, styles.buttonOpen]}
+          onPress={() => setModalVisible(true)}
+        >
+          <Text style={styles.textStyle}>{operation}</Text>
+        </Pressable>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  calendar: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#ecf0f1",
+    //today: { borderColor: "blue", borderWidth: 1 },
+    //selected: { backgroundColor: "blue" },
+    //selected_label: { color: "white" },
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+});
